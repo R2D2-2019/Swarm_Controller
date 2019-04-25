61231a03fa7a5c85271f18b570a2ead87f50133d
Tim <tim.vandijk@student.hu.nl>
2019-04-25 18:56:35 +0200

    rewrote CommandNode to inherit from dict

--- command_node.py
+++ command_node.py
@@ -1,5 +1,5 @@
 # Represents a node in a tree, this node knows its parent
-class CommandNode:
+class CommandNode(dict):
     def __init__(self,
                  name,
                  parent=None,
@@ -10,16 +10,9 @@ class CommandNode:
             parameter_list = list()
 
         self.name = name.upper()
-        self.children = dict()
         self.parent = parent
         self.parameter_list = parameter_list
         self.command_info = command_info
-
-    """
-    This function adds a child, or overrides it when it exists
-    """
-    def add_child(self, child):
-        self.children[child.name.upper()] = child
 
     """
     Gets all names of nodes in this branch in a list, ordered as root first
@@ -35,36 +28,3 @@ class CommandNode:
     """
     def set_parent(self, parent):
         self.parent = parent
-
-    """
-    Displays the children of this node
-    """
-    def show_children(self):
-        if self.children:
-            for child_key in self.children:
-                print(self.children[child_key].name)
-
-    """ 
-    Returns true if this node has give key (name) as a direct child, else returns false
-    """
-    def has_child(self, key):
-        return key.upper() in self.children
-
-    """
-    Gets the amount of children this node has
-    """
-    def get_child_amount(self):
-        return len(self.children)
-
-    """
-    Returns a child at the given key. Returns None if no key matches.
-    """
-    def get_child(self, key):
-        if key.upper() in self.children:
-            return self.children[key.upper()]
-        else:
-            return None
-
-
-    def __getitem__(self, index):
-        return self.children[index]
