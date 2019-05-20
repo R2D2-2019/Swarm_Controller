f7c8ff5998e1a2d074b0ded70da64e18010068d2
Timvdijk <tim.vandijk@student.hu.nl>
2019-05-20 11:51:07 +0200

    now we can't enter a command as a user; Commands and Categories now get handled correctly

--- module/input_handler.py
+++ module/input_handler.py
@@ -1,28 +1,32 @@
 
+from module.command_node import NodeType
+
 class input_handler():
     def __init__(self, cli_controller):
         self.cli_controller = cli_controller
-    
-    def handle_nonglobal_commands(self, user_word, user_command_list):
+
+    def handle_nonglobal_commands(self, user_word, user_command_list) -> bool:
         """
         Handles all non-global commands. Returns false if failed or if a function has been executed(in this case no other commands can be executed after).
         Returns true if another command can be executed after this one
         """
-        if user_word.upper() in self.cli_controller.current_node.keys():
-            self.cli_controller.current_node = self.cli_controller.current_node[user_word.upper()]
+
+        if user_word.upper() not in self.cli_controller.current_node.keys():
+            print("\tCommand '{}' not found, type 'help' for possible commands.".format(user_word))
+            return False
+
+        if self.cli_controller.current_node[user_word.upper()].type == NodeType.COMMAND:
+            if len(self.cli_controller.current_node[user_word.upper()].parameter_list) > 0:
+                print("\tCommand called with {} parameters: {}".format(
+                    len(user_command_list[user_command_list.index(user_word) + 1:]),
+                    "(" + ", ".join(user_command_list[user_command_list.index(user_word) + 1:]) + ")"
+                ))
             return True
 
-        elif len(self.cli_controller.current_node.parameter_list) > 0:
-            print("\tCommand called with {} parameters: {}".format(
-                len(user_command_list[user_command_list.index(user_word):]),
-                "(" + ",".join(user_command_list[user_command_list.index(user_word):]) + ")"
-            ))
-
-        elif user_word:
-            print("\tCommand {} not found, type \"help\" for possible commands.".format(user_word))
+        self.cli_controller.current_node = self.cli_controller.current_node[user_word.upper()]
         return False
 
-    
+
     def handle_new_input(self, input_commands):
         """
         Execute a command depending on text entered
@@ -31,10 +35,10 @@ class input_handler():
             # Step 1: Check for global commands
             if user_word.upper() in self.cli_controller.global_commands.keys():
                 self.cli_controller.global_commands[user_word.upper()]()
-                    
+
             # Step 2: Check for location(in tree structure) specific commands
             else:
-                if not self.handle_nonglobal_commands(user_word, input_commands):
+                if self.handle_nonglobal_commands(user_word, input_commands):
                     break
 
 
